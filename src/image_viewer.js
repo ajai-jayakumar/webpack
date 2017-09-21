f456201b9abcef6d6e5e293be0ac8aed333dbd94
ajai <ajai@arvindinternet.com>
2017-09-21 17:32:56 +0530

    Load js file on demand

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,13 +1,18 @@
-import big from '../assests/big.jpg';
 import small from '../assests/small.jpg';
-import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
 
-document.body.appendChild(image);
+    document.body.appendChild(image);
+};
 
-const bigImage = document.createElement('img');
-bigImage.src = big;
 
-document.body.appendChild(bigImage);
+//  Note :::
+//  export is not mandatory, if export is used then in index.js need to define 
+//  "module.default();" else just function call is enough.
+
+//import big from '../assests/big.jpg';
+// const bigImage = document.createElement('img');
+// bigImage.src = big;
+// document.body.appendChild(bigImage); 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import sum from './sum';
-import './image_viewer';
+import '../styles/image_viewer.css';
 
-const total = sum(10,5);
-console.log(total);
+const button = document.createElement('button');
+
+button.innerText = 'Click me';
+button.onclick = () => {
+    System.import('./image_viewer.js').then(module => {
+        module.default();
+    });
+};
+
+document.body.appendChild(button);
