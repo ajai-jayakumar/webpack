a8bc617b4133cc59bcb3b7c9ee40de10c2b1a89d
ajai <ajai@arvindinternet.com>
2017-09-21 16:29:40 +0530

    Added image assests to be load via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,32 @@ const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'build/'
     },
     module: {
         rules: [{
-            test: [/\.js$/],
-            exclude: [/node_modules/],
-            loader: 'babel-loader'
-        }, {
-            test:[/\.css$/],
-            use: ExtractTextPlugin.extract({
-                use: 'css-loader'
-            }),
-        }]
+                test: [/\.js$/],
+                exclude: [/node_modules/],
+                loader: 'babel-loader'
+            }, {
+                test: [/\.css$/],
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                }),
+            },
+            {
+                test: [/\.(jpe?g|png|gif|svg)$/],
+                use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 40000 // Create a new file if file size > 40kb else save the image inside bundle.js using base64
+                        }
+                    },
+                    'image-webpack-loader'
+                ]
+            }
+        ]
     },
     plugins: [
         new ExtractTextPlugin({
